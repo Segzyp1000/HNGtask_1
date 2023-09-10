13bfa14bbd824d11ac63b306d1c3d1dbad3d0ac8
Segzyp1000 <segunolowoyeye@gmail.com>
2023-09-10 13:10:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-let  time = new Date(Date.UTC(2023, 8, 8, 2, 29))
-document.getElementById('testId').innerHTML =time;
+document.addEventListener('DOMContentLoaded', function() {
+    // Get current date and time
+    const timeNow = new Date();
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDayOfWeek = daysOfWeek[timeNow.getUTCDay()];
+    const currentUTCTime = timeNow.getTime();
 
+    // Update elements with data-testid attributes
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent= `Current Day: ${currentDayOfWeek}`;
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = `Current UTC Time: ${currentUTCTime} milliseconds`;
+});
